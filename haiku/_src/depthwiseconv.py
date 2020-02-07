c5810bbdacc50f43c41813dc441e5cf75d239db0
Haiku Contributor <noreply@google.com>
2020-02-07 09:37:08 -0800

    Add 2D Depthwise convolution for Haiku. Tests demonstrate each filter acts on each channel separately.
    PiperOrigin-RevId: 293830699
    Change-Id: I023607a3d94602deb902b788b9e1787f9ba925dd

---
+++ haiku/_src/depthwiseconv.py
@@ -0,0 +1,122 @@
+# Lint as: python3
+# Copyright 2020 The Haiku Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+"""Depthwise Convolutional Haiku module."""
+
+from haiku._src import base
+from haiku._src import initializers
+from haiku._src import module
+from haiku._src import utils
+
+from jax import lax
+import jax.numpy as jnp
+import numpy as np
+
+DIMENSION_NUMBERS = {
+    1: ("NWC", "WIO", "NWC"),
+    2: ("NHWC", "HWIO", "NHWC"),
+    3: ("NDHWC", "DHWIO", "NDHWC")
+}
+
+DIMENSION_NUMBERS_NCSPATIAL = {
+    1: ("NCH", "HIO", "NCH"),
+    2: ("NCHW", "HWIO", "NCHW"),
+    3: ("NCDHW", "DHWIO", "NCDHW")
+}
+
+
+class DepthwiseConv2D(module.Module):
+  """2-D Depthwise Convolution Module."""
+  # TODO(tycai): Generalize to ConvND.
+
+  def __init__(self,
+               channel_multiplier,
+               kernel_shape,
+               stride=1,
+               padding="VALID",
+               with_bias=True,
+               w_init=None,
+               b_init=None,
+               data_format="channels_last",
+               name=None):
+    """Construct a 2D Depthwise Convolution.
+
+    Args:
+      channel_multiplier: Multiplicity of output channels. To keep the number of
+        output channels the same as the number of input channels, set 1.
+      kernel_shape: The shape of the kernel. Either an integer or a sequence of
+        length `num_spatial_dims`.
+      stride: Optional stride for the kernel. Either an integer or a sequence of
+        length `num_spatial_dims`. Defaults to 1.
+      padding: Optional padding algorithm. Either "VALID" or "SAME" or
+        a callable or sequence of callables of size `num_spatial_dims`. Any
+        callables must take a single integer argument equal to the effective
+        kernel size and return a list of two integers representing the padding
+        before and after. See haiku.pad.* for more details and example
+        functions. Defaults to "SAME". See:
+        https://www.tensorflow.org/xla/operation_semantics#conv_convolution.
+      with_bias: Whether to add a bias. By default, true.
+      w_init: Optional weight initialization. By default, truncated normal.
+      b_init: Optional bias initialization. By default, zeros.
+      data_format: The data format of the input.  Can be either
+        `channels_first`, `channels_last`, `N...C` or `NC...`. By default,
+        `channels_last`.
+      name: The name of the module.
+    """
+    super(DepthwiseConv2D, self).__init__(name=name)
+    self._kernel_shape = utils.replicate(kernel_shape, 2, "kernel_shape")
+    self._lhs_dilation = (1,) * len(self._kernel_shape)
+    self._rhs_dilation = (1,) * len(self._kernel_shape)
+    self._channel_multiplier = channel_multiplier
+    self._padding = padding
+    self._stride = utils.replicate(stride, 2, "strides")
+    self._data_format = data_format
+    self._channel_index = utils.get_channel_index(data_format)
+    self._with_bias = with_bias
+    self._w_init = w_init
+    self._b_init = b_init or jnp.zeros
+    self._num_spatial_dims = 2
+
+  def __call__(self, inputs):
+
+    channel_index = utils.get_channel_index(self._data_format)
+    weight_shape = self._kernel_shape + (1, self._channel_multiplier *
+                                         inputs.shape[channel_index])
+    fan_in_shape = np.prod(weight_shape[:-1])
+    stddev = 1. / np.sqrt(fan_in_shape)
+    w_init = self._w_init or initializers.TruncatedNormal(stddev=stddev)
+    w = base.get_parameter("w", weight_shape, inputs.dtype, init=w_init)
+    if self._channel_index == -1:
+      dn = DIMENSION_NUMBERS[self._num_spatial_dims]
+    else:
+      dn = DIMENSION_NUMBERS_NCSPATIAL[self._num_spatial_dims]
+    result = lax.conv_general_dilated(
+        inputs,
+        w,
+        self._stride,
+        self._padding,
+        self._lhs_dilation,
+        self._rhs_dilation,
+        dn,
+        feature_group_count=inputs.shape[channel_index])
+    if self._with_bias:
+      if channel_index == -1:
+        bias_shape = (self._channel_multiplier * inputs.shape[channel_index],)
+      else:
+        bias_shape = (self._channel_multiplier *
+                      inputs.shape[channel_index], 1, 1)
+      b = base.get_parameter("b", bias_shape, init=self._b_init)
+      result = result + b
+    return result
---
+++ haiku/_src/depthwiseconv_test.py
@@ -0,0 +1,165 @@
+# Lint as: python3
+# Copyright 2020 The Haiku Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+"""Tests for haiku._src.depthwiseconv."""
+
+from absl.testing import absltest
+from absl.testing import parameterized
+from haiku._src import base
+from haiku._src import depthwiseconv
+from haiku._src import initializers
+from jax import random
+import jax.numpy as jnp
+import numpy as np
+
+
+def create_constant_initializers(w, b, with_bias):
+  if with_bias:
+    return {
+        "w_init": initializers.Constant(w),
+        "b_init": initializers.Constant(b)
+    }
+  else:
+    return {"w_init": initializers.Constant(w)}
+
+
+class DepthwiseConv2DTest(parameterized.TestCase):
+
+  @parameterized.parameters(True, False)
+  def test_convolution(self, with_bias):
+    def f():
+      data = np.ones([1, 10, 10, 3])
+      data[0, :, :, 1] += 1
+      data[0, :, :, 2] += 2
+      data = jnp.array(data)
+      net = depthwiseconv.DepthwiseConv2D(
+          channel_multiplier=1,
+          kernel_shape=3,
+          stride=1,
+          padding="Valid",
+          with_bias=with_bias,
+          data_format="channels_last",
+          **create_constant_initializers(1.0, 1.0, with_bias))
+      return net(data)
+
+    init_fn, apply_fn = base.transform(f)
+    out = apply_fn(init_fn(random.PRNGKey(428)))
+    self.assertEqual(out.shape, (1, 8, 8, 3))
+    self.assertLen(np.unique(out[0, :, :, 0]), 1)
+    self.assertLen(np.unique(out[0, :, :, 1]), 1)
+    self.assertLen(np.unique(out[0, :, :, 2]), 1)
+    if with_bias:
+      self.assertEqual(np.unique(out[0, :, :, 0])[0], 1*3.0*3.0 +1)
+      self.assertEqual(np.unique(out[0, :, :, 1])[0], 2*3.0*3.0 +1)
+      self.assertEqual(np.unique(out[0, :, :, 2])[0], 3*3.0*3.0 +1)
+    else:
+      self.assertEqual(np.unique(out[0, :, :, 0])[0], 1*3.0*3.0)
+      self.assertEqual(np.unique(out[0, :, :, 1])[0], 2*3.0*3.0)
+      self.assertEqual(np.unique(out[0, :, :, 2])[0], 3*3.0*3.0)
+
+  @parameterized.parameters(True, False)
+  def test_padding(self, with_bias):
+    def f():
+      data = np.ones([1, 10, 10, 3])
+      data[0, :, :, 1] += 1
+      data[0, :, :, 2] += 2
+      data = jnp.array(data)
+      net = depthwiseconv.DepthwiseConv2D(
+          channel_multiplier=1,
+          kernel_shape=3,
+          stride=1,
+          padding="Same",
+          with_bias=with_bias,
+          data_format="channels_last",
+          **create_constant_initializers(1.0, 0.0, with_bias))
+      return net(data)
+
+    init_fn, apply_fn = base.transform(f)
+    out = apply_fn(init_fn(random.PRNGKey(428)))
+    self.assertEqual(out.shape, (1, 10, 10, 3))
+
+  @parameterized.parameters(True, False)
+  def test_channel_multiplier(self, with_bias):
+    def f():
+      data = np.ones([1, 10, 10, 3])
+      data[0, :, :, 1] += 1
+      data[0, :, :, 2] += 2
+      data = jnp.array(data)
+      net = depthwiseconv.DepthwiseConv2D(
+          channel_multiplier=3,
+          kernel_shape=3,
+          stride=1,
+          padding="Valid",
+          with_bias=with_bias,
+          data_format="channels_last",
+          **create_constant_initializers(1.0, 0.0, with_bias))
+      return net(data)
+
+    init_fn, apply_fn = base.transform(f)
+    out = apply_fn(init_fn(random.PRNGKey(428)))
+    self.assertEqual(out.shape, (1, 8, 8, 9))
+
+  @parameterized.parameters(True, False)
+  def test_channels_first(self, with_bias):
+    def f():
+      data = np.ones([1, 3, 10, 10])
+      data[0, 1, :, :] += 1
+      data[0, 2, :, :] += 2
+      data = jnp.array(data)
+      net = depthwiseconv.DepthwiseConv2D(
+          channel_multiplier=1,
+          kernel_shape=3,
+          stride=1,
+          padding="Valid",
+          with_bias=with_bias,
+          data_format="channels_first",
+          **create_constant_initializers(1.0, 1.0, with_bias))
+      return net(data)
+
+    init_fn, apply_fn = base.transform(f)
+    out = apply_fn(init_fn(random.PRNGKey(428)))
+    self.assertEqual(out.shape, (1, 3, 8, 8))
+    if with_bias:
+      self.assertEqual(np.unique(out[0, 0, :, :])[0], 1*3.0*3.0+1)
+      self.assertEqual(np.unique(out[0, 1, :, :])[0], 2*3.0*3.0+1)
+      self.assertEqual(np.unique(out[0, 2, :, :])[0], 3*3.0*3.0+1)
+    else:
+      self.assertEqual(np.unique(out[0, 0, :, :])[0], 1*3.0*3.0)
+      self.assertEqual(np.unique(out[0, 1, :, :])[0], 2*3.0*3.0)
+      self.assertEqual(np.unique(out[0, 2, :, :])[0], 3*3.0*3.0)
+
+  @parameterized.parameters(True, False)
+  def test_channels_first_mult(self, with_bias):
+    def f():
+      data = np.ones([1, 3, 10, 10])
+      data[0, 1, :, :] += 1
+      data[0, 2, :, :] += 2
+      data = jnp.array(data)
+      net = depthwiseconv.DepthwiseConv2D(
+          channel_multiplier=9,
+          kernel_shape=3,
+          stride=1,
+          padding="Valid",
+          with_bias=with_bias,
+          data_format="channels_first",
+          **create_constant_initializers(1.0, 0.0, with_bias))
+      return net(data)
+
+    init_fn, apply_fn = base.transform(f)
+    out = apply_fn(init_fn(random.PRNGKey(428)))
+    self.assertEqual(out.shape, (1, 27, 8, 8))
+
+if __name__ == "__main__":
+  absltest.main()

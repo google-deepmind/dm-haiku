c3919001d03bd9ef41a33c0d57eb8d586e0edb7e
Tom Hennigan <tomhennigan@google.com>
2020-02-24 11:29:43 -0800

    Add note re Sequential being limited in range of architectures.
    PiperOrigin-RevId: 296936275
    Change-Id: Iebf4c071725021a2889da6866fb7dd55d66e7027

--- haiku/_src/basic.py
+++ haiku/_src/basic.py
@@ -77,7 +77,31 @@ def multinomial(rng, logits, num_samples):
 
 # Common modules.
 class Sequential(module.Module):
-  """Sequentially calls the given list of layers."""
+  """Sequentially calls the given list of layers.
+
+  Note that `Sequential` is limited in the range of possible architectures
+  it can handle. This is a deliberate design decision; `Sequential` is only
+  meant to be used for the simple case of fusing together modules/ops where
+  the input of a particular module/op is the output of the previous one.
+
+  Another restriction is that it is not possible to have extra arguments in the
+  `__call__` method that are passed to the constituents of the module - for
+  example, if there is a `BatchNorm` module in `Sequential` and the user wishes
+  to switch the `is_training` flag. If this is the desired use case, the
+  recommended solution is to subclass `snt.Module` and implement `__call__`:
+
+      >>> class CustomModule(hk.Module):
+      ...   def __init__(self, name=None):
+      ...     super().__init__(name=name)
+      ...     self.conv2d = hk.Conv2D(32, 4, 2)
+      ...     self.bn = hk.BatchNorm()
+      ...
+      ...   def __call__(self, inputs, is_training):
+      ...     outputs = self.conv2d(inputs)
+      ...     outputs = self.bn(outputs, is_training=is_training)
+      ...     outputs = jax.nn.relu(outputs)
+      ...     return outputs
+  """
 
   def __init__(self,
                layers: Iterable[Callable[..., Any]] = None,

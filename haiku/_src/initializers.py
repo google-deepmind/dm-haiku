be987272a99ec2ec2ec6b6063390d9f1981c7ccc
joaogui <joaoguilhermearujo@gmail.com>
2020-05-20 16:06:44 -0300

    fix tabs

--- haiku/_src/initializers.py
+++ haiku/_src/initializers.py
@@ -256,27 +256,27 @@ class Orthogonal(Initializer):
     return jax.lax.convert_element_type(self.scale, dtype) * q_mat
 
 
-  class Identity(Initializer):
-    """Initializer that generates the identity matrix.
-    Constructs a 2D identity matrix or batches of these.
+class Identity(Initializer):
+  """Initializer that generates the identity matrix.
+  Constructs a 2D identity matrix or batches of these.
+  """
+
+  def __init__(self, gain: float = 1.0):
+    """Constructs an identity initializer.
+    Args:
+        gain: Multiplicative factor to apply to the identity matrix.
     """
+    self.gain = gain
 
-    def __init__(self, gain: float = 1.0):
-      """Constructs an identity initializer.
-      Args:
-         gain: Multiplicative factor to apply to the identity matrix.
-      """
-      self.gain = gain
-
-    def __call__(self, shape: Shape, dtype: DType) -> jnp.ndarray:
-      if len(shape) < 2:
-        raise ValueError('Identity initializer requires at least a 2D shape.')
-      elif len(shape) == 2:
-        initializer = jnp.eye(num_rows=shape[0], num_columns=shape[1], dtype=dtype)
-      else:
-        initializer = jnp.eye(
-          num_rows=shape[-2],
-          num_columns=shape[-1],
-          batch_shape=shape[:-2],
-          dtype=dtype)
-      return self.gain * initializer
+  def __call__(self, shape: Shape, dtype: DType) -> jnp.ndarray:
+    if len(shape) < 2:
+      raise ValueError('Identity initializer requires at least a 2D shape.')
+    elif len(shape) == 2:
+      initializer = jnp.eye(num_rows=shape[0], num_columns=shape[1], dtype=dtype)
+    else:
+      initializer = jnp.eye(
+        num_rows=shape[-2],
+        num_columns=shape[-1],
+        batch_shape=shape[:-2],
+        dtype=dtype)
+    return self.gain * initializer

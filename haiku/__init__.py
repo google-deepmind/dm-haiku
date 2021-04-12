ba62d1a4b44fc15e2b439b9c9d40a774dda7dcc1
Tom Hennigan <tomhennigan@google.com>
2021-04-12 04:47:22 -0700

    Bump to next dev version.
    PiperOrigin-RevId: 367978092
    Change-Id: I5ed1988f499a513b1f3a8248b2f376a898338517

--- haiku/__init__.py
+++ haiku/__init__.py
@@ -103,7 +103,7 @@ from haiku._src.transform import without_state
 from haiku._src.typing import Params
 from haiku._src.typing import State
 
-__version__ = "0.0.4"
+__version__ = "0.0.5.dev"
 
 __all__ = (
     "AvgPool",

eb2a9d155f82a88ebeb590d4b05d38d537fa28a0
Tom Hennigan <tomhennigan@google.com>
2021-10-18 03:42:01 -0700

    Improve documentation for `hk.lift`.
    Makes lift a function not an alias to a private module class, this provides a
    better summary on RTD.
    
    Additionally add an example from
    https://gist.github.com/tomhennigan/6f1237b5fb268a3d6d2391329ba2d051
    
    PiperOrigin-RevId: 403912320

--- haiku/_src/lift.py
+++ haiku/_src/lift.py
@@ -14,12 +14,24 @@
 # ==============================================================================
 """Lifting parameters in Haiku."""
 
+import types
+from typing import Any, Callable, MutableMapping
+
 from haiku._src import base
 from haiku._src import data_structures
 from haiku._src import module
+from haiku._src import transform
+
+# If you are forking replace this with `import haiku as hk`.
+hk = types.ModuleType("haiku")
+hk.Params = base.Params
+hk.Module = module.Module
+hk.running_init = transform.running_init
+hk.data_structures = data_structures
+del module, data_structures, transform
 
 
-def pack_into_dict(src, dst, prefix):
+def pack_into_dict(src: hk.Params, dst: MutableMapping[str, Any], prefix: str):
   """Puts items from src into dst, with an added prefix."""
   for key, value in src.items():
     new_key = f"{prefix}/{key}"
@@ -27,56 +39,70 @@ def pack_into_dict(src, dst, prefix):
     dst[new_key] = value
 
 
-def unpack_from_dict(src, prefix):
+def unpack_from_dict(src: hk.Params, prefix: str) -> hk.Params:
   """Returns pairs from src where key begins with prefix, cutting off prefix."""
-  result = dict()
-  for key, value in src.items():
-    if key.startswith(prefix):
-      result[key[len(prefix):]] = value
-  return data_structures.to_haiku_dict(result)
+  l = len(prefix)
+  out = {k[l:]: v for k, v in src.items() if k.startswith(prefix)}
+  return hk.data_structures.to_haiku_dict(out)
 
 
 # TODO(tycai): Accept state=True.
 # TODO(tycai): Make sure transformed functions have better names.
-class LiftingModule(module.Module):
-  r"""Lifts the given init function to a function in the current Haiku namespace.
+class LiftingModule(hk.Module):
+  """See :func:`lift`."""
+
+  def __init__(self, init_fn, name="lifted"):
+    super().__init__(name=name)
+    self._init_fn = init_fn
+
+  def __call__(self, *args, **kwargs):
+    outer_params = base.current_frame().params
+    if hk.running_init():
+      inner_params = self._init_fn(*args, **kwargs)
+      # Lift parameters into this transform's params_dict.
+      pack_into_dict(inner_params, outer_params, self.module_name)
+      return inner_params
+    else:
+      return unpack_from_dict(outer_params, f"{self.module_name}/")
+
+
+def lift(
+    init_fn: Callable[..., hk.Params],
+    name: str = "lifted",
+) -> Callable[..., hk.Params]:
+  r"""Lifts the given init fn to a function in the current Haiku namespace.
 
   During init, the returned callable will run the given ``init_fn``, and include
   the resulting params in the outer transform's dictionaries.
   During ``apply``, the returned callable will instead pull the relevant
   parameters from the outer transform's dictionaries.
 
-  Must be called inside :func:`~haiku.transform`\ , and be passed the ``init``
-  member of a :class:`~haiku.Transformed`\ .
+  Must be called inside :func:`transform`\ , and be passed the ``init``
+  member of a :class:`Transformed`\ .
 
   The user must ensure that the given ``init`` does not accidentally catch
-  modules from an outer :func:`~haiku.transform` via functional closure.
+  modules from an outer :func:`transform` via functional closure.
 
-  This is highly experimental and may be changed or removed at any time.
+  Example:
+
+    >>> def g(x):
+    ...   return hk.Linear(1, name='g_linear')(x)
+    >>> g = hk.transform(g)
+    >>> init_rng = hk.next_rng_key() if hk.running_init() else None
+    >>> x = jnp.ones([1, 1])
+    >>> params = hk.lift(g.init, name='f_lift')(init_rng, x)
+    >>> out = g.apply(params, None, x)
+
+  Args:
+    init_fn: The ``init`` function from an :class:`Transformed`\ .
+    name: A string name to prefix parameters with.
+
+  Returns:
+    A callable that during ``init`` injects parameter values into the outer
+    context and during ``apply`` reuses parameters from the outer context. In
+    both cases returns parameter values to be used with an ``apply`` function.
   """
-
-  def __init__(self, init_fn, name="lifted"):
-    """Initializes the LiftingModule.
-
-    Args:
-      init_fn: The ``init_fn`` from a :class:`~haiku.Transformed`. Must not be
-        stateful.
-      name: Module name.
-    """
-    super().__init__(name=name)
-    self._init_fn = init_fn
-
-  def __call__(self, *args, **kwargs):
-    frame = base.current_frame()
-    bundle_name = self.module_name
-    if base.params_frozen():
-      prefix = bundle_name + "/"
-      lifted_params = unpack_from_dict(frame.params, prefix)
-      return lifted_params
-    else:  # Inside init.
-      # Lift parameters into this transform's params_dict.
-      params = self._init_fn(*args, **kwargs)
-      pack_into_dict(params, frame.params, bundle_name)
-      return params
-
-lift = LiftingModule  # pylint: disable=invalid-name
+  base.assert_context("lift")
+  lifted = LiftingModule(init_fn, name=name)
+  # NOTE: Using lambda to avoid exposing module object.
+  return lambda *a, **k: lifted(*a, **k)  # pylint: disable=unnecessary-lambda

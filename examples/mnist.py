7f00f9c7b5f67147133a9d5375cf8eb9164c4647
John Aslanides <jaslanides@google.com>
2022-07-20 04:24:11 -0700

    Some improvements to the MNIST example.
    - Improve and clarify variable names throughout.
    - Improve docstrings and inline comments.
    - Use typed NamedTuple rather than dictionaries / tuples to hold data/state.
    - Make load-bearing magic numbers into constants (hyperparameters stay inlined).
    - Simplify/improve logging.
    
    PiperOrigin-RevId: 462109251

--- examples/mnist.py
+++ examples/mnist.py
@@ -12,9 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-"""MNIST classifier example."""
+"""A minimal MNIST classifier example."""
 
-from typing import Iterator, Mapping, Tuple
+from typing import Iterator, NamedTuple
 
 from absl import app
 import haiku as hk
@@ -24,17 +24,28 @@ import numpy as np
 import optax
 import tensorflow_datasets as tfds
 
-Batch = Mapping[str, np.ndarray]
+NUM_CLASSES = 10  # MNIST has 10 classes (hand-written digits).
 
 
-def net_fn(batch: Batch) -> jnp.ndarray:
+class Batch(NamedTuple):
+  image: np.ndarray  # [B, H, W, 1]
+  label: np.ndarray  # [B]
+
+
+class TrainingState(NamedTuple):
+  params: hk.Params
+  avg_params: hk.Params
+  opt_state: optax.OptState
+
+
+def net_fn(images: jnp.ndarray) -> jnp.ndarray:
   """Standard LeNet-300-100 MLP network."""
-  x = batch["image"].astype(jnp.float32) / 255.
+  x = images.astype(jnp.float32) / 255.
   mlp = hk.Sequential([
       hk.Flatten(),
       hk.Linear(300), jax.nn.relu,
       hk.Linear(100), jax.nn.relu,
-      hk.Linear(10),
+      hk.Linear(NUM_CLASSES),
   ])
   return mlp(x)
 
@@ -42,82 +53,78 @@ def net_fn(batch: Batch) -> jnp.ndarray:
 def load_dataset(
     split: str,
     *,
-    is_training: bool,
+    shuffle: bool,
     batch_size: int,
 ) -> Iterator[Batch]:
-  """Loads the dataset as a generator of batches."""
+  """Loads the MNIST dataset."""
   ds = tfds.load("mnist:3.*.*", split=split).cache().repeat()
-  if is_training:
+  if shuffle:
     ds = ds.shuffle(10 * batch_size, seed=0)
   ds = ds.batch(batch_size)
+  ds = ds.map(lambda x: Batch(**x))
   return iter(tfds.as_numpy(ds))
 
 
 def main(_):
-  # Make the network and optimiser.
-  net = hk.without_apply_rng(hk.transform(net_fn))
-  opt = optax.adam(1e-3)
+  # First, make the network and optimiser.
+  network = hk.without_apply_rng(hk.transform(net_fn))
+  optimiser = optax.adam(1e-3)
 
-  # Training loss (cross-entropy).
   def loss(params: hk.Params, batch: Batch) -> jnp.ndarray:
-    """Compute the loss of the network, including L2."""
-    logits = net.apply(params, batch)
-    labels = jax.nn.one_hot(batch["label"], 10)
+    """Cross-entropy classification loss, regularised by L2 weight decay."""
+    batch_size, *_ = batch.image.shape
+    logits = network.apply(params, batch.image)
+    labels = jax.nn.one_hot(batch.label, NUM_CLASSES)
 
-    l2_loss = 0.5 * sum(
+    l2_regulariser = 0.5 * sum(
         jnp.sum(jnp.square(p)) for p in jax.tree_util.tree_leaves(params))
-    softmax_xent = -jnp.sum(labels * jax.nn.log_softmax(logits))
-    softmax_xent /= labels.shape[0]
+    log_likelihood = jnp.sum(labels * jax.nn.log_softmax(logits))
 
-    return softmax_xent + 1e-4 * l2_loss
-
-  # Evaluation metric (classification accuracy).
-  @jax.jit
-  def accuracy(params: hk.Params, batch: Batch) -> jnp.ndarray:
-    predictions = net.apply(params, batch)
-    return jnp.mean(jnp.argmax(predictions, axis=-1) == batch["label"])
+    return -log_likelihood / batch_size + 1e-4 * l2_regulariser
 
   @jax.jit
-  def update(
-      params: hk.Params,
-      opt_state: optax.OptState,
-      batch: Batch,
-  ) -> Tuple[hk.Params, optax.OptState]:
+  def evaluate(params: hk.Params, batch: Batch) -> jnp.ndarray:
+    """Evaluation metric (classification accuracy)."""
+    logits = network.apply(params, batch.image)
+    predictions = jnp.argmax(logits, axis=-1)
+    return jnp.mean(predictions == batch.label)
+
+  @jax.jit
+  def update(state: TrainingState, batch: Batch) -> TrainingState:
     """Learning rule (stochastic gradient descent)."""
-    grads = jax.grad(loss)(params, batch)
-    updates, opt_state = opt.update(grads, opt_state)
-    new_params = optax.apply_updates(params, updates)
-    return new_params, opt_state
-
-  # We maintain avg_params, the exponential moving average of the "live" params.
-  # avg_params is used only for evaluation (cf. https://doi.org/10.1137/0330046)
-  @jax.jit
-  def ema_update(params, avg_params):
-    return optax.incremental_update(params, avg_params, step_size=0.001)
+    grads = jax.grad(loss)(state.params, batch)
+    updates, opt_state = optimiser.update(grads, state.opt_state)
+    params = optax.apply_updates(state.params, updates)
+    # Compute avg_params, the exponential moving average of the "live" params.
+    # We use this only for evaluation (cf. https://doi.org/10.1137/0330046).
+    avg_params = optax.incremental_update(
+        params, state.avg_params, step_size=0.001)
+    return TrainingState(params, avg_params, opt_state)
 
   # Make datasets.
-  train = load_dataset("train", is_training=True, batch_size=1000)
-  train_eval = load_dataset("train", is_training=False, batch_size=10000)
-  test_eval = load_dataset("test", is_training=False, batch_size=10000)
+  train_dataset = load_dataset("train", shuffle=True, batch_size=1_000)
+  eval_datasets = {
+      split: load_dataset(split, shuffle=False, batch_size=10_000)
+      for split in ("train", "test")
+  }
 
-  # Initialize network and optimiser; note we draw an input to get shapes.
-  params = avg_params = net.init(jax.random.PRNGKey(42), next(train))
-  opt_state = opt.init(params)
+  # Initialise network and optimiser; note we draw an input to get shapes.
+  initial_params = network.init(
+      jax.random.PRNGKey(seed=0), next(train_dataset).image)
+  initial_opt_state = optimiser.init(initial_params)
+  state = TrainingState(initial_params, initial_params, initial_opt_state)
 
-  # Train/eval loop.
-  for step in range(10001):
-    if step % 1000 == 0:
+  # Training & evaluation loop.
+  for step in range(3001):
+    if step % 100 == 0:
       # Periodically evaluate classification accuracy on train & test sets.
-      train_accuracy = accuracy(avg_params, next(train_eval))
-      test_accuracy = accuracy(avg_params, next(test_eval))
-      train_accuracy, test_accuracy = jax.device_get(
-          (train_accuracy, test_accuracy))
-      print(f"[Step {step}] Train / Test accuracy: "
-            f"{train_accuracy:.3f} / {test_accuracy:.3f}.")
+      # Note that each evaluation is only on a (large) batch.
+      for split, dataset in eval_datasets.items():
+        accuracy = np.array(evaluate(state.avg_params, next(dataset))).item()
+        print({"step": step, "split": split, "accuracy": f"{accuracy:.3f}"})
 
     # Do SGD on a batch of training examples.
-    params, opt_state = update(params, opt_state, next(train))
-    avg_params = ema_update(params, avg_params)
+    state = update(state, next(train_dataset))
 
 if __name__ == "__main__":
   app.run(main)

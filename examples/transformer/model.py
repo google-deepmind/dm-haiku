39ae7c904c5ed521fce665ed03b9e8f44d3b9ca5
David Lindner <dev@davidlindner.me>
2022-09-08 20:13:16 +0200

    Fix typo "example"

--- examples/transformer/model.py
+++ examples/transformer/model.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-"""Didactic exasmple of an autoregressive Transformer-based language model.
+"""Didactic example of an autoregressive Transformer-based language model.
 
 Glossary of shapes:
 - B: Batch size.
